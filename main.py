e370a47df0851b11e4b708b3a01456d1393161f0
Ricardo Brancas <anjomigel@gmail.com>
2018-10-25 16:18:11 +0100

    Completed basic implementation

--- main.py
+++ main.py
@@ -6,12 +6,23 @@ from nltk.corpus import stopwords
 from nltk.tokenize import RegexpTokenizer
 import csv
 from sklearn.preprocessing import normalize
-
+import argparse
+import sklearn.metrics.pairwise
 
 model = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
 
-with open('corpora/QuestoesConhecidas.txt', 'r') as file:
+parser = argparse.ArgumentParser(description='Classificador de questões relacionadas com cinema.')
+parser.add_argument('train_file', metavar='TRAINING')
+parser.add_argument('test_file', metavar='TESTING')
+
+args = parser.parse_args()
+
+classes = {}
+obs = {}
+
+with open(args.train_file, 'r') as file:
     reader = csv.reader(file, delimiter='\t')
+
     for row in reader:
         category = row[0]
         line = row[1]
@@ -25,5 +36,41 @@ with open('corpora/QuestoesConhecidas.txt', 'r') as file:
 
         sb = sb / np.linalg.norm(sb)
        
+        if category in classes:
+            classes[category] += sb
+            obs[category] += 1
+        else:
+            classes[category] = sb
+            obs[category] = 1
 
-        
+    for key, value in classes.items():
+        classes[key] = value / obs[key]
+
+with open(args.test_file, 'r') as file:
+    reader = csv.reader(file, delimiter='\t')
+
+    for row in reader:
+        line = row[0]
+        tokenizer = RegexpTokenizer(r'[A-Za-z]+')
+        tokens = tokenizer.tokenize(line)
+
+        sb = np.zeros((300))
+
+        filtered_words = filter(lambda token: token not in stopwords.words('english'), tokens)
+        for token in filtered_words:
+            sb += model[token]
+
+        sb = sb / np.linalg.norm(sb)
+
+        max_key = None
+        max_similarity = 0
+        for key, value in classes.items():
+            similarity = sklearn.metrics.pairwise.cosine_similarity(value.reshape(1, -1), sb.reshape(1, -1))
+
+            if similarity > max_similarity:
+                max_similarity = similarity
+                max_key = key
+
+        print(max_key)
+
+
---
+++ verify.py
@@ -0,0 +1,25 @@
+import sklearn.metrics
+from csv import reader
+import argparse
+
+parser = argparse.ArgumentParser(description='Classificador de questões relacionadas com cinema.')
+parser.add_argument('obt_file', metavar='OBTAINED')
+parser.add_argument('exp_file', metavar='EXPECTED')
+
+args = parser.parse_args()
+
+obt_file = open(args.obt_file, 'r')
+exp_file = open(args.exp_file, 'r')
+
+
+obt_vec = [line[0].strip() for line in reader(obt_file)]
+exp_vec = [line[0].strip() for line in reader(exp_file)]
+
+confusion_matrix = sklearn.metrics.confusion_matrix(obt_vec, exp_vec)
+
+print("Confusion matrix:\n " + str(confusion_matrix))
+
+
+
+
+

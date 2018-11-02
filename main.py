848c80835b3923f64149b89a73349cdcb16d4c2d
Ricardo Brancas <anjomigel@gmail.com>
2018-11-02 19:50:14 +0000

    Small refactoring

--- main.py
+++ main.py
@@ -10,17 +10,7 @@ import argparse
 import sklearn.metrics.pairwise
 import re
 
-def replaceNames(sentence, oldNames, newName):
-    for name in oldNames:
-        if name and len(name[0]) > 0:
-            if(name[0] in sentence):
-                sentence = re.sub(r'\b' + name[0] + r'\b', " " + newName + " ", sentence)
-    return sentence
-    
-
-model = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
-
-parser = argparse.ArgumentParser(description='Classificador de questões relacionadas com cinema.')
+parser = argparse.ArgumentParser(description='Classificador de perguntas relacionadas com cinema.')
 parser.add_argument('train_file', metavar='TRAINING')
 parser.add_argument('test_file', metavar='TESTING')
 
@@ -36,32 +26,50 @@ genreNames = list(csv.reader(open('recursos/list_genres.txt'), delimiter='\t'))
 jobNames = list(csv.reader(open('recursos/list_jobs.txt'), delimiter='\t'))
 peopleNames = list(csv.reader(open('recursos/list_people.txt'), delimiter='\t'))
 
+tokenizer = RegexpTokenizer(r'[A-Za-z]+')
+model = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
+
+
+def replaceNames(sentence, oldNames, newName):
+    for name in oldNames:
+        if name and len(name[0]) > 0:
+            if(name[0] in sentence):
+                sentence = re.sub(r'\b' + name[0] + r'\b', " " + newName + " ", sentence)
+    return sentence
+
+
+def process(line):
+
+    #Replace keywords
+    line = replaceNames(line, movieNames, 'movie')
+    line = replaceNames(line, characterNames, 'character')
+    line = replaceNames(line, genreNames, 'genre')
+    line = replaceNames(line, jobNames, 'job')
+    line = replaceNames(line, peopleNames, 'person')
+
+    tokens = tokenizer.tokenize(line)
+    filtered_tokens = filter(lambda token: token not in stopwords.words('english'), tokens)
+
+    sb = np.zeros((300))
+    for token in filtered_tokens:
+        if token in model:
+            sb += model[token]
+
+    norm = np.linalg.norm(sb)
+    if norm != 0:
+        sb = sb / np.linalg.norm(sb)
+
+    return sb
+
+
 with open(args.train_file, 'r') as file:
     reader = csv.reader(file, delimiter='\t')
 
     for row in reader:
         category = row[0]
-        line = row[1]
 
-        #Replace key words
-        line = replaceNames(line, movieNames, 'movie')
-        line = replaceNames(line, characterNames, 'character')
-        line = replaceNames(line, genreNames, 'genre')
-        line = replaceNames(line, jobNames, 'job')
-        line = replaceNames(line, peopleNames, 'person')
+        sb = process(row[1])
 
-        tokenizer = RegexpTokenizer(r'[A-Za-z]+')
-        tokens = tokenizer.tokenize(line)
-
-        sb = np.zeros((300))
-        filtered_words = filter(lambda token: token not in stopwords.words('english'), tokens)
-        for token in filtered_words:
-            if token in model:
-                sb += model[token]
-        norm = np.linalg.norm(sb)
-        if norm != 0:
-            sb = sb / np.linalg.norm(sb)
-       
         if category in classes:
             classes[category] += sb
             obs[category] += 1
@@ -69,44 +77,24 @@ with open(args.train_file, 'r') as file:
             classes[category] = sb
             obs[category] = 1
 
-    for key, value in classes.items():
-        classes[key] = value / obs[key]
+for key, value in classes.items():
+    classes[key] = value / obs[key]
 
 with open(args.test_file, 'r') as file:
     reader = csv.reader(file, delimiter='\t')
 
     for row in reader:
-        line = row[0]
-
-        #Replace key words
-        line = replaceNames(line, movieNames, 'movie')
-        line = replaceNames(line, characterNames, 'character')
-        line = replaceNames(line, genreNames, 'genre')
-        line = replaceNames(line, jobNames, 'job')
-        line = replaceNames(line, peopleNames, 'person')
-
-        tokenizer = RegexpTokenizer(r'[A-Za-z]+')
-        tokens = tokenizer.tokenize(line)
-
-        sb = np.zeros((300))
-
-        filtered_words = filter(lambda token: token not in stopwords.words('english'), tokens)
-        for token in filtered_words:
-            if token in model:
-                sb += model[token]
-
-        norm = np.linalg.norm(sb)
-        if norm != 0:
-            sb = sb / np.linalg.norm(sb)
+        sb = process(row[0])
+        reshaped_sb = sb.reshape(1, -1)
 
         max_key = None
         max_similarity = 0
-        for key, value in classes.items():
-            similarity = sklearn.metrics.pairwise.cosine_similarity(value.reshape(1, -1), sb.reshape(1, -1))
+        for category, value in classes.items():
+            similarity = sklearn.metrics.pairwise.cosine_similarity(value.reshape(1, -1), reshaped_sb)
 
             if similarity > max_similarity:
                 max_similarity = similarity
-                max_key = key
+                max_key = category
 
         print(max_key)
 
